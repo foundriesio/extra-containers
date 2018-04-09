3501f3a31a4a8f8aa0b7ff22b74ee84afd8f031d
Andy Doan <andy@opensourcefoundries.com>
2018-04-09 10:29:45 -0500

    jobserv-worker: Add a cold-reboot script
    This is useful in CI so you can add support to your device
    
    Signed-off-by: Andy Doan <andy@opensourcefoundries.com>

---
+++ jobserv-worker/cold-reboot.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -e
+
+worker=$(cat /srv/jobserv-worker/settings.conf | grep hostname | cut -d= -f2 | tr -d '[:space:]')
+
+if [ "$worker" = "andy-rpi5" ] ; then
+	exec curl -X PUT http://reckless:5000/1/reboot
+fi
+if [ "$worker" = "doanac-rpi3-64-2" ] ; then
+	exec curl -X PUT http://reckless:5000/2/reboot
+fi
+
+echo "Unknown/support worker: $worker"
+exit 1
