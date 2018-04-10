5ecfd4f72d0608ca4e1e1ee0e866d0b1974dc51a
Andy Doan <andy@opensourcefoundries.com>
2018-04-09 21:53:54 -0500

    jobserv-worker: Add doanc's qemu device
    Signed-off-by: Andy Doan <andy@opensourcefoundries.com>

--- jobserv-worker/cold-reboot.sh
+++ jobserv-worker/cold-reboot.sh
@@ -10,6 +10,9 @@ fi
 if [ "$worker" = "doanac-rpi3-64-2" ] ; then
 	exec curl -X PUT http://reckless:5000/2/reboot
 fi
+if [ "$worker" = "doanac-x86-qemu-1" ] ; then
+	exec curl -X PUT http://reckless:5001/doanac-x86-qemu-1/reboot
+fi
 
 echo "Unknown/support worker: $worker"
 exit 1
