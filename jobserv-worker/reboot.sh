6518a40638d0c4a220fd698fda05a0c87b8318cb
Andy Doan <andy@foundries.io>
2018-08-09 11:05:38 -0500

    Remove the jobserv-worker
    This was only ever used internally and I've now stopped using it.
    
    Signed-off-by: Andy Doan <andy@foundries.io>

--- jobserv-worker/reboot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -e
-
-sync
-
-echo s > /sysrq
-sleep 1
-echo b > /sysrq
--- jobserv-worker/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -e
-
-docker run --privileged -d --name jobserv-worker \
-	--restart unless-stopped \
-	-v /proc/sysrq-trigger:/sysrq \
-	registry.foundries.io/development/microplatforms/linux/extra-containers/jobserv-worker
