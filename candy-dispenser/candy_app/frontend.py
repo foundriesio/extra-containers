d8ef95defae3966142e60e7bfbec2c1aa594cadb
Tyler Baker <tyler@foundries.io>
2019-02-05 13:05:59 -0800

    candy-dispenser: add basic flask app
    Basic bootstrap flask application to control an LWM2M candy machine.
    
    Signed-off-by: Tyler Baker <tyler@foundries.io>

---
+++ candy-dispenser/candy_app/frontend.py
@@ -0,0 +1,54 @@
+# This contains our frontend; since it is a bit messy to use the @app.route
+# decorator style when using application factories, all of our routes are
+# inside blueprints. This is the front-facing blueprint.
+#
+# You can find out more about blueprints at
+# http://flask.pocoo.org/docs/blueprints/
+
+from flask import Blueprint, render_template, flash, redirect, url_for
+from flask_bootstrap import __version__ as FLASK_BOOTSTRAP_VERSION
+from markupsafe import escape
+
+import os
+import time
+import json
+import requests
+
+frontend = Blueprint('frontend', __name__)
+
+headers = { 'Content-Type': 'application/json'}
+
+def post(url):
+    response = requests.post(url, headers=headers)
+    if response.status_code == 200 or 201:
+        return True
+    else:
+        return False
+
+def put(url, data):
+    response = requests.put(url, data=json.dumps(data), headers=headers)
+    if response.status_code in (200, 201):
+        return True
+    else:
+        return False
+
+def toggle(host, client, state):
+    url = '%s/api/clients/%s/3311/0/5850' % (host, client)
+    nextstate = {'id': 5850, 'value': state}
+    return put(url, nextstate)
+
+# Our index-page just shows a quick explanation. Check out the template
+# "templates/index.html" documentation for more details.
+@frontend.route('/')
+def index():
+    return render_template('index.html')
+
+@frontend.route("/dispense/", methods=['POST'])
+def dispense():
+    message = "Dispensing Candy..."
+    host = os.environ['HOST']
+    client = os.environ['CLIENT']
+    toggle(host, client, True)
+    time.sleep(0.5)
+    toggle(host,client, False)
+    return render_template('index.html', message=message);
